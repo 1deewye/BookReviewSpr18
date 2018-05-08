861ae89ca15d9ecbd8bfe9100feff3b21970a255
1deewye <35506588+1deewye@users.noreply.github.com>
2018-05-08 10:56:13 -0700

    Class Example 5/8 add BookEntry

---
+++ BookReviewSpr18/Controllers/NewBookController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookReviewSpr18.Models;
+
+namespace BookReviewSpr18.Controllers
+{
+    public class NewBookController : Controller
+    {
+        // GET: NewBook
+        public ActionResult Index()
+        {
+            if(Session["ReviewerKey"]==null)
+            {
+                //Message m = new Message("You must be logged in to add a book");
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "BookTitle, BookEntryDate, BookISBN")]Book b)
+        {
+            b.BookEntryDate = DateTime.Now;
+            BookReviewDbEntities db = new BookReviewDbEntities();
+            db.Books.Add(b);
+            db.SaveChanges();
+            Message m = new Message("Book has been entered");
+            return View("Result", m);
+        }
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
